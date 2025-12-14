320dc81c203710a27bd37b6aca49528cd271189b
lord-vector <r.anuj.agrawal@gmail.com>
2025-12-14 23:41:06 +0530

    sa

--- Application Load Balancer with Cloud Armor/anuj.sh
+++ Application Load Balancer with Cloud Armor/anuj.sh
@@ -1,10 +1,10 @@
 
 
+export REPO_NAME=$(gcloud config get-value project)
+export DOCKER_IMAGE=$(gcloud config get-value project)
+export TAG_NAME=$(gcloud config get-value project)
+export ZONE=$(gcloud config get-value project)
 export REGION="${ZONE%-*}"
-export REPO_NAME='valkyrie-docker'
-export DOCKER_IMAGE='valkyrie-app'
-export TAG_NAME='v0.0.1'
-export ZONE='us-west1-b'
 
 gcloud auth list
 source <(gsutil cat gs://cloud-training/gsp318/marking/setup_marking_v2.sh)
